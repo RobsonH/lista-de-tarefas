02ee982f8f531431aa9b94656629a6b819553985
Robson Hammer <robsonads531@gmail.com>
2018-11-21 20:39:09 -0400

    Botao inserir

--- client/assets/js/api.js
+++ client/assets/js/api.js
@@ -9,7 +9,10 @@ function listarTarefas(filtro){
             if (this.readyState ==4) {
                 if (this.status == 200) {
                     resolve(JSON.parse(this.response));
-                }else{
+                }else if (this.status ==404){
+                    resolve(null);
+                }
+                else{
                     reject("Erro ao conectar ao servidor");
                 }
             }
@@ -19,4 +22,25 @@ function listarTarefas(filtro){
         requisicao.send();
     });
 
+}
+
+function inserirTarefa(tarefa){
+    return new Promisse(function(resolve, reject){
+        let url = 'http://localhost:3010/api/v1/tarefas/f/';
+        let requisicao = new XMLHttpRequest();
+
+        requisicao.onreadystatechange = function (){
+            if (this.readyState == 4){
+                if(this.status == 201){
+                    resolve(JSON.parse(this.responseText));
+                }else{
+                    reject('Erro ao enviar dados ao servidor');
+                }
+            }
+        }
+
+        requisicao.open("POST", url, true);
+        requisicao.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        requisicao.send(JSON.stringify(tarefa));
+    });
 }
--- client/assets/js/funcoes.js
+++ client/assets/js/funcoes.js
@@ -1,39 +1,74 @@
 
+//Caso o botão pesquisar seja clicado
+document.querySelector('#btn-buscar').addEventListener('click', function(envent){
+    //Previne que o formulario seja submetido
+    event.preventDefault();
+    //Montar formulario
+    montarPainel();
+});
+
 //Funçao para montar os cartões
 function montarPainel(){
     //Mapeando o painel de tarefas do DOM
     let painelTarefas = document.querySelector('#painelTarefas');
     painelTarefas.innerHTML = '';
+//Mapeando a caixa de busca, capturando o texto da busca
+
+let filtro = document.querySelector('#texto-busca').value;
 
 //Espera o resultado da função listarTarefas()
-    let promise = listarTarefas("");
+    let promise = listarTarefas(filtro);
     promise
     //Caso o resultado seja processado
         .then(function(response){
-            //console.log(response);
-
-            response.forEach(function(item){
-                //Criando o cartão
-                let cartao = document.createElement('div');
-                cartao.className = 'card';
-                cartao.innerHTML = `
-                    <div class="card">
-                    <div class="card-body">
-                      <div>
-                        <span class="card-subtitle mb-2 text-muted">${item.data}</span>
-                      </div>
-                      <p class="card-text">${item.descricao}</p>
-                    </div>
-                </div>
-              `;
-              //Adicionando o cartão no painel de tarefas
-              painelTarefas.appendChild(cartao);
-
-            });
-
+//Caso não seja encontrado as tarefas
+            if (response == null){
+                monstrarMensagem('Nenhuma tarefa encontrada para esta busca!','d');
+            }else{
+//Caso seja encontradas as tarefas  via API
+                
+                            response.forEach(function(item){
+                                //Criando o cartão
+                                let cartao = document.createElement('div');
+                                cartao.className = 'card';
+                                cartao.innerHTML = `
+                                    <div class="card">
+                                    <div class="card-body">
+                                      <div>
+                                        <span class="card-subtitle mb-2 text-muted">${item.data}</span>
+                                      </div>
+                                      <p class="card-text">${item.descricao}</p>
+                                    </div>
+                                </div>
+                              `;
+                              //Adicionando o cartão no painel de tarefas
+                              painelTarefas.appendChild(cartao);
+                
+                            });
+            }
         })
         //Caso o resultado não seja processado
         .catch(function(erro){
             console.log(erro);
         });    
 }
+//Quando o botão adicionar tarefa for clicado
+document.querySelector('#btn-adicionar').addEventListener('click', function(event){
+    event.preventDefault();
+    //Mostrar o modal
+    $('#modal').modal('show');
+
+    //Mostrar o layout do modal
+    document.querySelector('#btn-inserir').classList.remove('nao-mostrar');
+    document.querySelector('#btn-alterar').classList.add('nao-mostrar');
+    document.querySelector('#btn-deletar').classList.add('nao-mostrar');
+    document.querySelector('.modal-title').innerHTML = 'Inserir nova tarefa';
+
+    //Setando o foco no campo descricao-tarefa
+    document.querySelector('#descricao-tarefa').focus();
+
+    //Limpando os campos do formulario
+    document.querySelector('#descricao-tarefa').value = '';
+    document.querySelector('#data-tarefa').value = '';
+
+})
